a4f1d80669b2da387706595eccce00e3af3753c3
Maxime <emixampons@gmail.com>
2021-03-25 19:49:27 +0100

    improved on the event system, which would handle clicks if piston weren't weird

--- src/components.rs
+++ src/components.rs
@@ -1,3 +1,4 @@
+
 use std::collections::HashMap;
 use std::any::Any;
 
@@ -7,19 +8,31 @@ use piston_window::*;
 
 pub trait SceneObject {
 	fn render<G: Graphics>(&self, context: &Context, graphics: &mut G);
-	fn update(&self, e: &Event, extra: &HashMap<&str, Box<dyn Any + 'static>>);
+	fn update(&mut self, e: &Event, extra: &HashMap<&str, Box<dyn Any + 'static>>);
 }
 
 pub struct GUIButton {
 	pub coords: [f64 ; 4],
 	pub color: [f32 ; 4],
+	pub clicked: bool,
 }
 impl SceneObject for GUIButton {
 	fn render<G: Graphics>(&self, context: &Context, graphics: &mut G) {
 		rectangle(self.color, self.coords, context.transform, graphics);
 	}
-	fn update(&self, e: &Event, extra: &HashMap<&str, Box<dyn Any + 'static>>) {
-
+	fn update(&mut self, e: &Event, extra: &HashMap<&str, Box<dyn Any + 'static>>) {
+		let clicked = extra.get("clicked").unwrap().downcast_ref::<bool>().unwrap();
+		let mousePos = extra.get("mousePos").unwrap().downcast_ref::<Option<[f64 ; 2]>>().unwrap();
+		if mousePos.is_none() {
+			return ();
+		}
+		let mouse_pos = mousePos.unwrap();
+		if mouse_pos[0] > self.coords[0] && mouse_pos[0] < self.coords[0] + self.coords[2] && mouse_pos[1] > self.coords[1] && mouse_pos[1] < self.coords[1] + self.coords[3] {
+			self.clicked = *clicked;
+			if self.clicked {
+				self.color = [0.0, 1.0, 0.0, 0.0];
+			}
+		}
 	}
 }
 
--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,27 @@ use piston_window::*;
 use components::{SceneObject, GUIButton};
 
 fn main() {
-	let mut window: PistonWindow = WindowSettings::new("Hello Calculator", [640, 480]).exit_on_esc(true).build().unwrap();
-	let buttons: Vec<Box<GUIButton>> = vec![Box::new(GUIButton{ coords: [0.0, 0.0, 100.0, 100.0], color: [1.0, 0.0, 0.0, 1.0] }), Box::new(GUIButton{ coords: [0.0, 200.0, 100.0, 100.0], color: [1.0, 0.0, 0.0, 1.0] })];
-	let mut mousePos: Option<[f64 ; 2]> = None;
+	let opengl = OpenGL::V3_2;
+	let mut window: PistonWindow = WindowSettings::new("Hello Calculator", [640, 480]).graphics_api(opengl).exit_on_esc(true).build().unwrap();
+	let mut buttons: Vec<Box<GUIButton>> = vec![Box::new(GUIButton{ coords: [0.0, 0.0, 100.0, 100.0], color: [1.0, 0.0, 0.0, 1.0], clicked: false }), Box::new(GUIButton{ coords: [0.0, 200.0, 100.0, 100.0], color: [1.0, 0.0, 0.0, 1.0], clicked: false })];
+	let mut mousePos: [f64 ; 2] = [-1.0, -1.0];
 	let mut mousePressed: bool = false;
 	let mut click: bool = false;
 	while let Some(event) = window.next() {
 		window.draw_2d(&(event), |context, graphics, device| {
 						click = false;
 						if let Some(pos) = event.mouse_cursor_args() {
-							mousePos = Some(pos);
+							mousePos = pos;
 						}
 						if let Some(button) = event.press_args() {
+							println!("pressed");
 							if button == Button::Mouse(MouseButton::Left) {
 								mousePressed = true;
 							}
 						}
 						if let Some(button) = event.release_args() {
 							if button == Button::Mouse(MouseButton::Left) {
+								println!("released");
 								if mousePressed {
 									click = true;
 								}
@@ -37,12 +40,12 @@ fn main() {
 						if let Some(args) = event.update_args() {
 							let extra = {
 								let mut object: HashMap<&str, Box<dyn Any + 'static>> = HashMap::new();
-								object.insert("dt", Box::new(args.dt));
+								object.insert("dt", Box::new(0)); //args.dt
 								object.insert("clicked", Box::new(click));
 								object.insert("mousePos", Box::new(mousePos));
 								object
 							};
-							for button in buttons.iter() {
+							for button in buttons.get_mut(0) {
 								button.update(&(event), &extra);
 							}
 						}
